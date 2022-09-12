adcc84b29bef37d4e3769c30b70bfade3bb29ff4
Aras emr <64031332+arasemr12@users.noreply.github.com>
2022-09-13 00:05:48 +0300

    Api link fix.

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ function makerequest(){
 makerequest();
 
 function getposts(){
-    axios.get("http://45.136.4.58/api/users/6317ab9fe62c7d0d87d1c387").then((res) => {
+    axios.get("http://45.136.4.58:8080/api/users/6317ab9fe62c7d0d87d1c387").then((res) => {
         posts = res.data;
         setTimeout(() => {
             getposts();
