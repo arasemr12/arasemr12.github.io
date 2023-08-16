fc75989efebf55fcfe98b9983457f34ef10e0a51
Aras emr <arasemr1234@gmail.com>
2023-08-16 23:38:09 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Loaduser from "./Loaduser";
 import Index from ".";
 import More from "./more";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setLoading, setRepos, setUser } from "./redux/slices/userSlice";
 
 const router = createBrowserRouter([
     {
@@ -15,12 +17,68 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-    return (
-        <>
-            <Loaduser/>
-            <RouterProvider router={router} />
-        </>
-    )
+  const dispatch = useDispatch();
+
+  const msToSec = (ms) => {
+    let seconds = Math.floor(ms / 1000);
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds%60;
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+  }
+
+  const getUser = async() => {
+    let res = await fetch("https://api.lanyard.rest/v1/users/441221465019514881");
+    let data = await res.json();
+
+    dispatch(setLoading({
+      loading:false
+    }));
+
+    data = data.data;
+
+    if(!data.discord_user.avatar) data.imageurl = `https://cdn.discordapp.com/embed/avatars/1.png`;
+    else data.imageurl = `https://cdn.discordapp.com/avatars/${data.discord_user.id}/${data.discord_user.avatar}.png`;
+    
+    if(data.spotify){
+        let start = data.spotify.timestamps.start;
+        let end = data.spotify.timestamps.end;
+        data.spotify.timestamps.start = msToSec(Date.now()-start);
+        data.spotify.timestamps.end = msToSec(end-start);
+        data.spotify.timestamps.played = ((Date.now()-start) / (end-start))*100;
+    }
+
+    dispatch(setUser({
+        user:data
+    }));
+
+    setTimeout(getUser, 1000);
+  };
+
+  const getRepos = async() => {
+    let res = await fetch("https://api.github.com/users/arasemr12/repos?type=owner");
+    let json = await res.json();
+
+    if(json.message.includes("API rate limit")) return;
+
+    dispatch(setRepos({
+        repos:json
+    }));
+  };
+
+  const init = async() => {
+    getUser();
+    getRepos();
+  };
+
+  useEffect(() => {
+    init();
+  },[]);
+
+  return (
+      <>
+        <RouterProvider router={router} />
+      </>
+  )
 };
 
 export default App;
--- src/more.jsx
+++ src/more.jsx
@@ -2,19 +2,19 @@ import { useSelector } from "react-redux";
 import Back from "./components/Back";
 
 function More() {
-  let user = useSelector(state => state.user);
-  let repos = user.repos;
-  user = user.user;
+  let user = useSelector(state => state.user).user;
+  let repos = useSelector(state => state.user).repos;
+  let loading = useSelector(state => state.user).loading;
 
-  if(repos.length <= 0) return (
+  if(loading) return (
     <div className="fixed flex items-center justify-center w-full h-full z-50">
-      <span className="block w-10 h-10 border-[1px] border-gray-600 border-t-gray-400 rounded-full animate-spin z-50"></span>
+      <span className="block w-5 h-5 border-[1px] border-gray-600 border-t-gray-400 rounded-full animate-spin z-50"></span>
     </div>
   );
 
   return (
     <>
-      <div className="lg:w-2/3 w-full h-full flex flex-col items-center p-4 gap-5 other_anim">
+      <div className="lg:w-2/3 w-full h-full flex flex-col items-center p-4 gap-5 other_anim overflow-auto">
           <div className="w-full flex flex-col gap-1">
               <span className="text-2xl">Who I am?</span>
               <span>I am a self-taught web/desktop developer. I am also learning game/os development.</span>
@@ -25,18 +25,25 @@ function More() {
                 <div className="skill">Html</div>
                 <div className="skill">Css</div>
                 <div className="skill">Javascript</div>
-                <div className="skill">Node.JS</div>
-                <div className="skill">TypeScript</div>
-                <div className="skill">Nuxt.JS</div>
+                <div className="skill">Node</div>
+                <div className="skill">Typescript</div>
+                <div className="skill">Nuxt</div>
+                <div className="skill">Vue</div>
                 <div className="skill">React</div>
-                <div className="skill">TailwindCSS</div>
+                <div className="skill">Next</div>
+                <div className="skill">Tailwind</div>
                 <div className="skill">C++</div>
                 <div className="skill">C</div>
+                <div className="skill">GO</div>
               </div>
           </div>
           <div className="w-full flex flex-col gap-2 py-4">
               <span className="text-2xl">My open source projects.</span>
               <div className="w-full flex flex-col lg:flex-row flex-wrap gap-5">
+                {repos.length < 1 ? (
+                <span className="text-xl font-bold text-center w-full">
+                  No repo!
+                </span>) : (<></>)}
                   {repos.map((repo) => (
                       <a key={repo.id} href={repo.html_url} target="_blank" className="w-full lg:w-[45%] hover:scale-105 border-[1px] border-gray-700 duration-300 flex flex-col p-4 rounded-lg bg-gray-800">
                           <span>{repo.name}</span>
