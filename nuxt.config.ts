ef98ab80d5c79d1e013eb8af7aed112e02a3f848
Aras emr <arasemr1234@gmail.com>
2024-01-14 22:18:45 +0300

    gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,21 @@ export default defineNuxtConfig({
   },
   app:{
     head:{
+      script:[
+        {
+          async:true,
+          src:"https://www.googletagmanager.com/gtag/js?id=G-BZ9R74CT2Y",
+        },
+        {
+          innerHTML:`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-BZ9R74CT2Y');
+          `
+        }
+      ],
       link:[
         {
             rel:"stylesheet",
